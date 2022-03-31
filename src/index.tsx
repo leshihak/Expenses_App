c58027478df7b12c5e9327114dec26debecc7858
Nataliia Leshchak <n.leshchak@ualwo8689nbl.levi9.com>
2022-03-31 12:30:40 +0300

    update packages and react

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,26 @@
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './hooks/useAuth';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.render(
-  <StrictMode>
+const container = document.getElementById('root');
+
+if (container) {
+  const root = ReactDOMClient.createRoot(container);
+
+  root.render(
     <AuthProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
       <ToastContainer />
     </AuthProvider>
-  </StrictMode>,
-  document.getElementById('root')
-);
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
